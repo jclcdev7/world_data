ab034a3b1a0025ca7ecf0982d427cbc56ce1708a
jclcdev7 <jclc.dev7@gmail.com>
2018-05-14 22:45:47 +0200

    first commit

--- getcurlnotes_def.js
+++ getcurlnotes_def.js
@@ -1,3 +1,4 @@
-const execSync = require('child_process').execSync;
+// too muchconst
+execSync = require('child_process').execSync;
 
 execSync('curl -k --GET https://www.cia.gov/library/publications/resources/the-world-factbook/docs/notesanddefs.html > notesanddefs/notes_def.html');
